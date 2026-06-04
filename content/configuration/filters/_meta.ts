db288fafb399857638ffc50690c69bb7db102776
viveleroi <botsko@gmail.com>
2026-06-04 08:17:31 -0700

    Adds new filter documentation

---
+++ content/configuration/filters/_meta.ts
@@ -0,0 +1,11 @@
+import type { MetaRecord } from 'nextra'
+ 
+const meta: MetaRecord = {
+    index: {},
+    conditions: {},
+    ordered: {},
+    grouped: {},
+    examples: {}
+}
+ 
+export default meta
